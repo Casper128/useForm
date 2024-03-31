5ea66a7c852fb0fe34cbb3d6cfb0cc21ed09e958
Darwin Osorio <darwinosorio286@gmail.com>
2024-03-30 22:57:45 -0500

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Form } from './components'
+function App() {
+  return (
+    <>
+      <div className='container-xxl'>
+        <Form/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,43 @@
+import { useForm } from '../../hooks';
+import './form.css'
+
+const defaultValues = { firstName: '' } //no deseo usar esto para inicializar mi formulario
+export const Form = () => {
+    const {
+        formErrors,
+        registerField,
+        handleSubmit,
+    } = useForm(defaultValues);
+
+    const formSubmit = (data) => {
+        console.log("Form Data:", data);
+    };
+
+    return (
+        <form onSubmit={(event) => handleSubmit(event, formSubmit)}>
+            <input
+                type="text"
+                {...registerField("firstName", { //deseo usar este nombre que se envia aqui
+                    required: 'This input is required.',
+                    pattern: {
+                        value: /^[A-Za-z]+$/u,
+                        message: "This input only allow letters."
+                    },
+                    minLength: {
+                        value: 1,
+                        message: "This input must exceed 8 characters"
+                    },
+                    maxLength: {
+                        value: 20,
+                        message: "This input max 20 characters"
+                    },
+                })}
+            />
+            <div className="form-input_button">
+                <button type="submit" >
+                    Submit
+                    </button>
+            </div>
+        </form>
+    )
+}
